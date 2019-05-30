60d8842a27da478f57e70ad9d23d45a3c98ce206
kisumutalent <homiemusa@gmail.com>
2019-05-30 11:45:33 +0300

    Update Destory APIViews

--- project/posts/api/views.py
+++ project/posts/api/views.py
@@ -1,4 +1,9 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+	DestroyAPIView,
+	ListAPIView,
+	UpdateAPIView,
+	RetrieveAPIView,
+	)
 
 from posts.models import Post
 from .serializers import PostDetailSerializer, PostListSerializer
@@ -7,6 +12,18 @@ class PostDetailAPIVeiw(RetrieveAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostDetailSerializer
 	lookup_field = 'slug'
+
+class PostUpdateAPIVeiw(UpdateAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	lookup_field = 'slug'
+
+
+class PostDeleteAPIVeiw(DestroyAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	lookup_field = 'slug'
+
 
 class PostListAPIVeiw(ListAPIView):
 	queryset = Post.objects.all()
