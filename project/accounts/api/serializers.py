0377f3006df01bce6ec6e8bb04f5f5055e6200fd
kisumutalent <homiemusa@gmail.com>
2019-07-25 19:16:18 +0300

    Validating Password and email api

--- project/accounts/api/serializers.py
+++ project/accounts/api/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 
 from rest_framework.serializers import (
+	EmailField,
 	ModelSerializer,
 	HyperlinkedIdentityField,
 	SerializerMethodField,
@@ -11,16 +12,41 @@ from rest_framework.serializers import (
 User = get_user_model()
 
 class UserCreateSerializer(ModelSerializer):
+	email = EmailField(label = 'Email Address')
+	email2 = EmailField(label='Confirm Email')
 	class Meta:
 		model = User 
 		fields = [
 			'username',
 			'email',
+			'email2',
 			'password',
 		]
 		extra_kwargs = {"password":
 											{"write_only": True}
 										}
+
+	# Compare Email address if they exists.
+	def validete(self, data):
+		# email = data['email']
+		# user_qs = User.object.filter(email=email)
+		# if user_qs.exists():
+		# 	raise ValidationError("This Email Address is already registered")
+		return data
+
+	# Validates Emails 
+	def validate_email2(self, value):
+		data = self.get_initial()
+		email1 = data.get("email")
+		email2 = value
+		if email1 != email2:
+			raise ValidationError("Emails must match")
+
+		# Compare Email address if they exists.
+		user_qs = User.objects.filter(email=email2)
+		if user_qs.exists():
+			raise ValidationError("This Email Address is already registered")
+		return value
 										
 	# Save a user in the model
 	def create(self, validate_data):
